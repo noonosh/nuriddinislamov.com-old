f31ec15efde0f08dd4b83482617ca72b28dacf24
Nuriddin Islamov <nuriddin.k2003@gmail.com>
2021-11-13 22:33:52 +0000

    Complete Hero component mobile-first approached

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Hero from './components/Hero';
-import About from './components/About';
-import Skills from './components/Skills';
-import Links from './components/Links';
-import Contact from './components/Contact';
+// import About from './components/About';
+// import Skills from './components/Skills';
+// import Links from './components/Links';
+// import Contact from './components/Contact';
 
 function App() {
 	return (
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,7 +1,66 @@
 import React from 'react';
+import Typewriter from 'typewriter-effect';
 
 const Hero = () => {
-	return <h1>This is a Hero section</h1>;
+	return (
+		<>
+			<header>
+				<div className='grid grid-cols-2 font-thin tracking-widest text-xl'>
+					<div className='m-9'>
+						<span>@nuriddinislamov</span>
+					</div>
+					<div className='m-9 text-right'>
+						<img
+							src='../icons/hamburger.svg'
+							alt='Hamburger icon'
+							className='hidden'
+						/>
+						<ul>
+							<li className='pb-3'>
+								<a href='#about'>About</a>
+							</li>
+							<li className='pb-3'>
+								<a href='#skills'>Skills</a>
+							</li>
+							<li className='pb-3'>
+								<a href='#contact'>Contact</a>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</header>
+			<section>
+				<div className='mt-8 px-5 text-6xl font-bold pb-4'>
+					<Typewriter
+						onInit={typewriter => {
+							typewriter
+								.typeString('Hi ')
+								.pasteString('<code>&#128075;</code></br>')
+								.typeString('<span>I am Nuriddin,</span>')
+								.pauseFor(2000)
+								.pasteString('<br/>')
+								.typeString(
+									'<span class="text-gray-400 software-engineer">Software Deve</span>',
+								)
+								.pauseFor(1000)
+								.deleteChars(4)
+								.typeString(
+									'<span class="text-gray-400 software-engineer">Engineer</span>',
+								)
+								.start();
+						}}
+					/>
+					{/* <h1>
+							Hi 👋
+							<br />
+							I'm Nuriddin,
+							<br />
+							Software Developer
+						</h1> */}
+				</div>
+			</section>
+		</>
+	);
 };
 
 export default Hero;
