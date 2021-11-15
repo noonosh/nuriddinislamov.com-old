d2417e0aebd30415106569dc40cef7262db48ea1
Nuriddin Islamov <nuriddin.k2003@gmail.com>
2021-11-15 20:45:17 +0000

    Update card component

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,11 +2,11 @@ import React from 'react';
 
 const Card = ({ image, listItems, alt }) => {
 	return (
-		<div className='border-4 w-72 mx-auto my-6 rounded-xl shadow p-4 flex-column flex justify-start'>
+		<div className='w-72 mx-auto my-6 rounded-xl shadow bg-white p-4 flex-column flex justify-start'>
 			<div className='self-center mb-2'>
 				<img src={image} alt={alt} className='rounded-full' />
 			</div>
-			<div className='pl-2 font-extralight tracking-wide'>
+			<div className='pl-4 font-extralight tracking-wide'>
 				<ul className='list-disc'>
 					{listItems.map(listItem => (
 						<li>{listItem}</li>
