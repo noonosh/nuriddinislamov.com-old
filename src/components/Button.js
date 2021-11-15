c9743adb54feb15c6be5c2bad30ad7b77df4b2ec
Nuriddin Islamov <nuriddin.k2003@gmail.com>
2021-11-15 17:26:58 +0000

    Update button component

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,27 +3,9 @@ import { Link } from 'react-router-dom';
 
 const Button = ({ text, route }) => {
 	return (
-		<div>
+		<div className='inline-block m-2'>
 			<Link
-				className=' 
-				px-4 py-2
-				border
-				border-gray-400
-				rounded-2xl
-				uppercase
-				shadow
-				bg-none
-				font-thin
-				tracking-widest
-				text-gray-800
-				text-xs
-				sm:text-base
-				transform
-				transition
-				hover:bg-gray-200
-				hover:text-current
-				active:-translate-y-1.5
-				active:bg-gray-300'
+				className='px-3 py-2 rounded-xl uppercase font-thin tracking-widest text-sm lg:text-lg focus:outline-none focus:ring focus:ring-gray-300 focus:ring-offset-2 hover:bg-gray-400 hover:text-white active:bg-gray-500 active:text-white transform active:translate-y-1.5'
 				to={route}>
 				{text}
 			</Link>
