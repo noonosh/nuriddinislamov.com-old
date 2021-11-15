56b15f1b10437eb7f7c0174f0b2cd1242a53c5f8
Nuriddin Islamov <nuriddin.k2003@gmail.com>
2021-11-15 22:31:51 +0000

    Refactor button styles

--- src/components/Button.js
+++ src/components/Button.js
@@ -5,7 +5,7 @@ const Button = ({ text, route }) => {
 	return (
 		<div className='inline-block m-2'>
 			<Link
-				className='px-3 py-2 rounded-xl uppercase font-thin tracking-widest text-sm lg:text-lg focus:outline-none focus:ring focus:ring-gray-300 focus:ring-offset-2 hover:bg-gray-400 hover:text-white active:bg-gray-500 active:text-white transform active:translate-y-1.5'
+				className='px-3 py-2 rounded-xl uppercase font-thin tracking-widest text-sm lg:text-lg hover:bg-gray-400 hover:text-white hover:transition-colors duration-300 active:bg-gray-500 active:text-white transform transition'
 				to={route}>
 				{text}
 			</Link>
