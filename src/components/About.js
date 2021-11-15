9ad924bda5a3449b377e2bcb944c373d208f9e4d
Nuriddin Islamov <nuriddin.k2003@gmail.com>
2021-11-15 18:55:14 +0000

    Complete About page and update Footrr

--- src/components/About.js
+++ src/components/About.js
@@ -16,23 +16,13 @@ const sentences = [
 
 const About = () => {
 	return (
-		<div className='container mx-auto'>
+		<div className='page-container'>
 			<Navbar />
 			<section>
-				<h1 className='mb-4 px-4 font-thin text-center text-6xl tracking-widest text-gray-400 lg:hidden filter drop-shadow-lg'>
+				<h1 className='m-2 p-2 font-thin text-center text-6xl tracking-widest text-gray-400 opacity-40 lg:hidden'>
 					About
 				</h1>
-
-				<div
-					className='
-								text-2xl
-								font-bold
-								mx-6
-								text-center
-								lg:flex
-								lg:justify-center
-								lg:text-6xl
-								lg:mb-12'>
+				<div className='mb-2 py-2 text-2xl text-gray-900 font-bold flex flex-wrap justify-center lg:text-5xl'>
 					<div>I'm&nbsp;</div>
 					<Typewriter
 						options={{
@@ -42,82 +32,37 @@ const About = () => {
 						}}
 					/>
 				</div>
-
-				<div
-					className='
-				bg-green-300
-				p-4
-				rounded
-				shadow
-				z-50'>
-					<div
-						className='w-24 h-24
-							lg:w-64
-							lg:h-64
-							mx-6
-							bg-blue-300
-							lg:bg-none
-							rounded-full
-							lg:rounded-lg
-							float-left'>
-						<img src={image} alt='Smiling face memoji' />
+				<div className='lg:my-12 max-w-sm lg:max-w-4xl mx-auto rounded-2xl shadow bg-white lg:flex'>
+					<div className='lg:flex-shrink-0'>
+						<img
+							src={image}
+							alt='Smiling face memoji'
+							className='h-72 w-full lg:w-72 lg:h-full object-scale-down'
+						/>
 					</div>
-
-					<div
-						className='
-											px-4
-											leading-8
-											text-lg
-											font-regular
-											text-xl
-											lg:text-xl
-											'>
-						<p style={{ 'text-indent': '1.5rem' }}>
+					<div className='p-8'>
+						<p
+							class='leading-relaxed'
+							style={{ 'text-indent': '1.5rem' }}>
 							Lorem Ipsum is simply dummy text of the printing and
 							typesetting industry. Lorem Ipsum has been the
-							industry's standard dummy text ever since the 1500s,
-							when an unknown printer took a galley of type and
-							scrambled it to make a type specimen book. It has
-							survived not only five centuries, but also the leap
-							into electronic typesetting, remaining essentially
-							unchanged.
+							industry's standard dummy text <strong>ever</strong>{' '}
+							since the 1500s, when an unknown printer took a
+							galley of type and scrambled it to make a type
+							specimen book. It has{' '}
+							<strong>survived not only five centuries</strong>,
+							but also the leap into electronic typesetting,
+							remaining essentially unchanged.
 						</p>
 					</div>
 				</div>
 
-				<div
-					className='
-					hidden
-					lg:inline-block
-					absolute
-					right-0	
-					ml-36	
-					'>
-					<h1
-						className='
-							mx-4
-							px-12
-							font-thin
-							text-center
-							text-9xl
-							tracking-widest
-							text-gray-400
-							opacity-30
-							transform
-							rotate-90
-							w-32
-							-mt-32
-							z-0
-							'>
-						About
-					</h1>
-				</div>
-				<div className='flex flex-wrap-reverse my-6 px-4 justify-center lg:justify-start'>
-					<div className='my-3 px-2'>
-						<Button text='< Back to main page' route='/' />
+				<div className='flex justify-center mb-16 mt-4 lg:pb-24'>
+					<div>
+						<Button text='Back to main' route='/' />
 					</div>
-					<div className='my-3 px-2'>
-						<Button text='Next >' route='/skills' />
+					<div>
+						<Button text='Next' route='/skills' />
 					</div>
 				</div>
 			</section>
