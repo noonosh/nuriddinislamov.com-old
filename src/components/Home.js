91877020810fa10972ba2d110b14743372d9b798
Nuriddin Islamov <nuriddin.k2003@gmail.com>
2021-11-15 16:55:58 +0000

    Major update for layout (flex)

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,49 +1,43 @@
 import React from 'react';
-import Navbar from './Navbar';
 import Typewriter from 'typewriter-effect';
 import { Navigate } from 'react-router-dom';
+import Navbar from './Navbar';
 
 const Home = () => {
 	return (
-		<main className='container mx-auto'>
-			<Navbar />
-			<section>
-				<div className='my-16 mx-4 p-4 text-5xl font-bold lg:text-7xl lg:m-8 lg:p-6'>
-					{/* <h1 className='lg:ml-24'>
-						Hi &#128075;
-						<br />I am Nuriddin, <br />
-						<span className='text-gray-400'>Software Engineer</span>
-					</h1> */}
-					<Typewriter
-						onInit={typewriter => {
-							typewriter
-								.pauseFor(1000)
-								.typeString('Hi ')
-								.pasteString('<code>&#128075;</code><br/>')
-								.typeString('I am Nuriddin')
-								.typeString('..')
-								.pauseFor(1000)
-								.deleteAll(50)
-								.typeString(
-									"Bruuh...<br/>That doesn't <br/> seem  good looking ",
-								)
-								.pasteString('<code>&#129300;</code>')
-								.pauseFor(1500)
-								.deleteAll(30)
-								.callFunction(state => {
-									// hide cursor
-									state.elements.cursor.style.display =
-										'none';
-								})
-								.callFunction(() => {
-									return <Navigate to='/about' />;
-								})
-								.start();
-						}}
-					/>
-				</div>
-			</section>
-		</main>
+		<div className='page-container'>
+			<div>
+				<Navbar />
+			</div>
+			<div className='px-4 text-5xl flex-grow justify-center font-bold lg:text-7xl lg:m-8'>
+				<Typewriter
+					onInit={typewriter => {
+						typewriter
+							.pauseFor(1000)
+							.typeString('Hi ')
+							.pasteString('<code>&#128075;</code><br/>')
+							.typeString('I am Nuriddin')
+							.typeString('..')
+							.pauseFor(1000)
+							.deleteAll(50)
+							.typeString(
+								"Bruuh...<br/>That doesn't <br/> seem  good looking ",
+							)
+							.pasteString('<code>&#129300;</code>')
+							.pauseFor(1500)
+							.deleteAll(30)
+							.callFunction(state => {
+								// hide cursor
+								state.elements.cursor.style.display = 'none';
+							})
+							.callFunction(() => {
+								return <Navigate to='/about' />;
+							})
+							.start();
+					}}
+				/>
+			</div>
+		</div>
 	);
 };
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@ module.exports = {
 	purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
 	darkMode: false, // or 'media' or 'class'
 	theme: {
-		extend: {},
+		extend: {
+			container: {
+				center: true,
+			},
+		},
 	},
 	variants: {
 		extend: {
