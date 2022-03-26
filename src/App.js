00fd4544078579adb07c6fa381a22369b8c9d967
Nuriddin Islamov <nuriddin.k2003@gmail.com>
2022-03-26 20:55:51 +0500

    .

---
+++ src/App.js
@@ -0,0 +1,16 @@
+function App() {
+	return (
+		<div className='flex justify-center min-h-screen px-3'>
+			<div className='self-center text-center'>
+				<h1 className='text-8xl font-bold'>
+					<span className='text-lime-400'>Will</span>{' '}
+					<span className='text-teal-400'>you</span>{' '}
+					<span className='text-red-600 underline'>marry</span>{' '}
+					<span className='text-zinc-600'>me?</span>{' '}
+				</h1>
+			</div>
+		</div>
+	);
+}
+
+export default App;
