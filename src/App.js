528060710fdf5439a3a293835e7fe31db4496ce2
Nuriddin Islamov <nuriddin.k2003@gmail.com>
2022-04-13 08:48:04 +0100

    comment out imports of components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import NavBar from './components/NavBar';
-import Home from './components/Home';
-import About from './components/About';
-import Work from './components/Work';
-import Contact from './components/Contact.js';
-import Footer from './components/Footer';
+// import NavBar from './components/NavBar';
+// import Home from './components/Home';
+// import About from './components/About';
+// import Work from './components/Work';
+// import Contact from './components/Contact.js';
+// import Footer from './components/Footer';
 import Maintenance from './components/Maintenance';
 
 const App = () => {
