b5ec42247c111ceae01d8fdc0e7f95ce195243df
Nuriddin Islamov <nuriddin.k2003@gmail.com>
2022-04-14 13:54:50 +0100

    Add an ad block

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
 import React from 'react';
-// import NavBar from './components/NavBar';
-// import Home from './components/Home';
-// import About from './components/About';
-// import Work from './components/Work';
-// import Contact from './components/Contact.js';
-// import Footer from './components/Footer';
+import NavBar from './components/NavBar';
+import Home from './components/Home';
+import About from './components/About';
+import Work from './components/Work';
+import Contact from './components/Contact.js';
+import Footer from './components/Footer';
 import Maintenance from './components/Maintenance';
 
 const App = () => {
 	return (
-		<div className='App'>
-			{/* <NavBar />
+		<div>
+			<Maintenance />
+		</div>
+		/* <main className='text-gray-400 bg-gray-900 body-font'>
+			<NavBar />
 			<Home />
 			<About />
 			<Work />
 			<Contact />
-			<Footer /> */}
-			<Maintenance />
-		</div>
+			<Footer />
+			
+		</main> */
 	);
 };
 
