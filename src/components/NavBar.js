220821b1f62f11da96d90d725dba1b006e7eb505
Nuriddin Islamov <nuriddin.k2003@gmail.com>
2022-04-16 17:55:45 +0100

    Refactor navigation bar header

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,30 +1,31 @@
 import React from 'react';
 import useWindowDimensions from '../windowDimensions';
+import { Link } from 'react-router-dom';
 
 const NavBar = () => {
 	const { width } = useWindowDimensions();
 
 	return (
-		<header className='py-6 md:py-8 px-24 sticky top-0 bg-slate-200 w-screen navbar-fixed-top'>
-			<div className='flex flex-col items-center md:flex-row md:justify-between'>
+		<header className='pt-6 md:pt-8 px-24 sticky top-0 bg-white w-screen navbar-fixed-top'>
+			<div className='flex flex-col items-center md:flex-row md:justify-between border-b pb-8'>
 				<div className='text-center md:text-left'>
 					<h1 className='font-extrabold text-2xl md:text-4xl md:self-center md:mb-0 inline-block active:underline underline-offset-2 clickable'>
-						<a href='/'>{width > 768 ? 'Nuriddin Islamov' : 'Nuriddin'}</a>
+						<Link to='/'>{width >= 768 ? 'Nuriddin Islamov' : 'Nuriddin'}</Link>
 					</h1>
 					<p className='font-light text-base md:text-base'>
 						Jr. Software Engineer
 					</p>
 				</div>
 
-				<ul className='md:flex flex-col items-center md:flex-row md:text-xl md:font-light md:tracking-wide hidden whitespace-nowrap'>
-					<li className='ml-8 hover:underline underline-offset-4 hover:text-gray-600'>
-						<a href='/about'>About me</a>
+				<ul className='md:flex text-lg font-medium hidden whitespace-nowrap'>
+					<li className='nav_item'>
+						<Link to='/about'>About me</Link>
 					</li>
-					<li className='ml-8 hover:underline underline-offset-4 hover:text-gray-600'>
-						<a href='/download'>Projects</a>
+					<li className='nav_item'>
+						<Link to='/projects'>Projects</Link>
 					</li>
-					<li className='ml-8 hover:underline underline-offset-4 hover:text-gray-600'>
-						<a href='/contact'>Get in touch</a>
+					<li className='nav_item'>
+						<Link to='/reach-out'>Get in touch</Link>
 					</li>
 				</ul>
 			</div>
