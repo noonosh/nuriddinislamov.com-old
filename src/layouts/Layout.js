e0844daec02a4232114801d65c76968fb71aecc9
Nuriddin Islamov <nuriddin.k2003@gmail.com>
2022-04-16 17:56:31 +0100

    full screen layout

--- src/layouts/Layout.js
+++ src/layouts/Layout.js
@@ -4,15 +4,19 @@ import Footer from '../components/Footer';
 
 function Layout({ children, id }) {
 	return (
-		<>
-			<NavBar />
+		<div className='flex flex-col h-screen'>
+			<div className='flex-none'>
+				<NavBar />
+			</div>
 			<section
-				className='container mx-auto h-screen w-full px-24 py-12'
+				className='container flex-1 mx-auto h-full w-5/6 px-2 md:px-6 py-8'
 				id={id}>
 				{children}
 			</section>
-			<Footer />
-		</>
+			<div className='flex-none'>
+				<Footer />
+			</div>
+		</div>
 	);
 }
 
