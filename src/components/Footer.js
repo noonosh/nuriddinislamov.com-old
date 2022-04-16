a11b86d452bb8edebac06456025c85170b7a6d94
Nuriddin Islamov <nuriddin.k2003@gmail.com>
2022-04-16 14:12:53 +0100

    Footer and NavBar header

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,81 @@
 import React from 'react';
+import useWindowDimensions from '../windowDimensions';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+import {
+	faGithub,
+	faTwitter,
+	faLinkedinIn,
+	faDev,
+} from '@fortawesome/free-brands-svg-icons';
 
 const Footer = () => {
-	return <div>Footer</div>;
+	const { width } = useWindowDimensions();
+
+	return (
+		<footer className='py-6 bg-slate-100 px-8'>
+			<div className='flex justify-between text-center'>
+				<div className='text-center text-gray-500 font-light tracking-tight'>
+					© Nuriddin Islamov, {new Date().getFullYear()}
+				</div>
+
+				<div className=''>
+					{width > 768 ? (
+						<ul className='flex justify-between'>
+							<li className='nav_item'>
+								<a
+									href='https://github.com/nuriddinislamov'
+									target='_blank'
+									rel='noopener'
+									className='hover:text-sky-600'>
+									<FontAwesomeIcon icon={faGithub} className='text-2xl' />
+								</a>
+							</li>
+							<li className='nav_item'>
+								<a
+									href='https://twitter.com/nuriddinislamov'
+									target='_blank'
+									rel='noopener'
+									className='hover:text-sky-600'>
+									<FontAwesomeIcon icon={faTwitter} className='text-2xl' />
+								</a>
+							</li>
+							<li className='nav_item'>
+								<a
+									href='https://linkedin.com/in/nuriddinislamov'
+									target='_blank'
+									rel='noopener'
+									className='hover:text-sky-600'>
+									<FontAwesomeIcon icon={faLinkedinIn} className='text-2xl' />
+								</a>
+							</li>
+							<li className='nav_item'>
+								<a
+									href='https://blog.nuriddinislamov.com'
+									target='_blank'
+									rel='noopener'
+									className='hover:text-sky-600'>
+									<FontAwesomeIcon icon={faDev} className='text-2xl' />
+								</a>
+							</li>
+						</ul>
+					) : (
+						<ul>
+							<li className='nav_item'>
+								<a
+									href='https://github.com/nuriddinislamov'
+									target='_blank'
+									rel='noopener'
+									className='hover:text-sky-600'>
+									<FontAwesomeIcon icon={faGithub} className='text-2xl' />
+								</a>
+							</li>
+						</ul>
+					)}
+				</div>
+			</div>
+		</footer>
+	);
 };
 
 export default Footer;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,7 +1,35 @@
 import React from 'react';
+import useWindowDimensions from '../windowDimensions';
 
 const NavBar = () => {
-	return <div>NavBar</div>;
+	const { width } = useWindowDimensions();
+
+	return (
+		<header className='py-6 md:py-8 px-24 sticky top-0 bg-slate-200 w-screen navbar-fixed-top'>
+			<div className='flex flex-col items-center md:flex-row md:justify-between'>
+				<div className='text-center md:text-left'>
+					<h1 className='font-extrabold text-2xl md:text-4xl md:self-center md:mb-0 inline-block active:underline underline-offset-2 clickable'>
+						<a href='/'>{width > 768 ? 'Nuriddin Islamov' : 'Nuriddin'}</a>
+					</h1>
+					<p className='font-light text-base md:text-base'>
+						Jr. Software Engineer
+					</p>
+				</div>
+
+				<ul className='md:flex flex-col items-center md:flex-row md:text-xl md:font-light md:tracking-wide hidden whitespace-nowrap'>
+					<li className='nav_item hover:underline underline-offset-4 hover:text-gray-600'>
+						<a href='/about'>About me</a>
+					</li>
+					<li className='nav_item hover:underline underline-offset-4 hover:text-gray-600'>
+						<a href='/download'>Projects</a>
+					</li>
+					<li className='nav_item hover:underline underline-offset-4 hover:text-gray-600'>
+						<a href='/contact'>Get in touch</a>
+					</li>
+				</ul>
+			</div>
+		</header>
+	);
 };
 
 export default NavBar;
