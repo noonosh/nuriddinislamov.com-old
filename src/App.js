2c106cd601a9311d38406f91f522655c3c952361
Nuriddin Islamov <nuriddin.k2003@gmail.com>
2022-04-17 23:35:40 +0100

    Enable all links

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import Home from './components/Home';
 import About from './components/About';
-// import Work from './components/Work';
-// import Contact from './components/Contact.js';
+import Work from './components/Work';
+import Contact from './components/Contact.js';
 import NotFound from './components/404';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
@@ -13,8 +13,8 @@ const App = () => {
 				<Routes>
 					<Route path='/' element={<Home />} />
 					<Route path='/me' element={<About />} />
-					{/* <Route path='/projects' element={<Work />} />
-					<Route path='/reach-out' element={<Contact />} /> */}
+					<Route path='/projects' element={<Work />} />
+					<Route path='/reach-out' element={<Contact />} />
 					<Route path='*' element={<NotFound />} />
 				</Routes>
 			</BrowserRouter>
