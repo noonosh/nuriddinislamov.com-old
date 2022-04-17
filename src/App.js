8fa41bcbc1052138b660d56b6e881f1ab7545d56
Nuriddin Islamov <nuriddin.k2003@gmail.com>
2022-04-17 23:35:12 +0100

    fix a bug with layot

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Home from './components/Home';
-// import About from './components/About';
+import About from './components/About';
 // import Work from './components/Work';
 // import Contact from './components/Contact.js';
 import NotFound from './components/404';
@@ -12,8 +12,8 @@ const App = () => {
 			<BrowserRouter>
 				<Routes>
 					<Route path='/' element={<Home />} />
-					{/* <Route path='/me' element={<About />} />
-					<Route path='/projects' element={<Work />} />
+					<Route path='/me' element={<About />} />
+					{/* <Route path='/projects' element={<Work />} />
 					<Route path='/reach-out' element={<Contact />} /> */}
 					<Route path='*' element={<NotFound />} />
 				</Routes>
