e9e21998ce490b6546c0ea59350c3c1e6acd8645
Nuriddin Islamov <nuriddin.k2003@gmail.com>
2022-04-18 01:24:08 +0100

    Update navigation buttons

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,14 +2,17 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 function Navigation(props) {
+	const navClasses =
+		'border-2 border-gray-600 px-4 py-2 my-2 rounded hover:bg-gray-800 hover:border-gray-800 hover:text-white whitespace-nowrap text-xl font-bold';
+
 	return (
 		<div className='h-20 flex flex-wrap-reverse justify-between items-center px-12 md:hidden'>
-			{props.prev.path == '' ? null : (
-				<div className='border-2 border-gray-600 px-4 py-2 my-2 rounded-3xl whitespace-nowrap'>
+			{props.prev.path === '' ? null : (
+				<div className={navClasses}>
 					<Link to={props.prev.path}>&larr; {props.prev.name}</Link>
 				</div>
 			)}
-			<div className='border-2 border-gray-600 px-4 py-2 rounded-3xl whitespace-nowrap'>
+			<div className={navClasses}>
 				<Link to={props.next.path}>{props.next.name} &rarr;</Link>
 			</div>
 		</div>
