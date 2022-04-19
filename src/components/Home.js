7293552a0005dccfae51c57c04c497e274be5744
Nuriddin Islamov <nuriddin.k2003@gmail.com>
2022-04-19 16:00:41 +0100

    🔗 Add a github fork bookmark on home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,6 +6,12 @@ import Navigation from './Navigation';
 const Home = () => {
 	return (
 		<Layout id='home'>
+			<a
+				className='bg-red-600 absolute rotate-90 block -right-24 text-center top-20 bold font-bold text-sm text-white shadow-md py-1 px-24 z-20'
+				href='http://github.com/nuriddinislamov/nuriddinislamov.com'
+				id='github'>
+				Fork me on Github
+			</a>
 			<div className='flex flex-col h-full'>
 				<div className='flex-auto'>
 					<Maintenance />
