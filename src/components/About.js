1b0ec05ae3099dfd8a8f25548c9b6b69c34f2d81
Nuriddin Islamov <nuriddin.k2003@gmail.com>
2022-04-22 18:39:15 +0100

    render md file into html with react-markdown

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,27 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
 import Layout from '../layouts/Layout';
 import Navigation from './Navigation';
+import ReactMarkdown from 'react-markdown';
+// import remarkGfm from 'remark-gfm';
+import aboutPage from '../pages/about.md';
 
 const About = () => {
+	const [content, setContent] = useState('');
+
+	useEffect(() => {
+		fetch(aboutPage)
+			.then(res => res.text())
+			.then(text => setContent(text));
+	}, []);
+
 	return (
 		<Layout id='about'>
-			<div className='flex flex-col h-full'>
-				<div className='flex-auto'>ABOUT PAGE</div>
+			<div className='flex flex-col h-full prose prose-slate'>
+				<div className='flex-auto overflow-scroll'>
+					<ReactMarkdown children={content} />
+				</div>
+
 				<Navigation
 					prev={{ name: 'home', path: '/' }}
 					next={{ name: 'work', path: '/projects' }}
---
+++ src/pages/about.md
@@ -0,0 +1,5 @@
+# This is a h1 heading
+
+## Followed by an h2 heading
+
+**BOLD TEXT**
