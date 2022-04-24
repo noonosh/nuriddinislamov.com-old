b8c6706e5a4377d3f4006718ec89beb451f7c773
Nuriddin Islamov <nuriddin.k2003@gmail.com>
2022-04-24 13:20:17 +0100

    home page waves landing

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,6 +5,7 @@ import Navigation from './Navigation';
 import ReactMarkdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
 import homePage from '../pages/home.md';
+import useWindowDimensions from '../windowDimensions';
 
 class Clock extends React.Component {
 	constructor(props) {
@@ -37,6 +38,7 @@ class Clock extends React.Component {
 
 const Home = () => {
 	const [content, setContent] = useState('');
+	const { width } = useWindowDimensions();
 
 	useEffect(() => {
 		fetch(homePage)
@@ -58,14 +60,16 @@ const Home = () => {
 			<div class='text-2xl font-bold'>
 				<Clock />
 			</div>
-			<div class='absolute bottom-1 left-0 -z-10 w-screen'>
-				<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 1440 320'>
-					<path
-						fill='#f1f5f9'
-						fill-opacity='1'
-						d='M0,96L48,117.3C96,139,192,181,288,202.7C384,224,480,224,576,202.7C672,181,768,139,864,128C960,117,1056,139,1152,133.3C1248,128,1344,96,1392,80L1440,64L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z'></path>
-				</svg>
-			</div>
+			{width >= 768 ? (
+				<div class='absolute bottom-1 left-0 -z-10 w-screen'>
+					<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 1440 320'>
+						<path
+							fill='#f1f5f9'
+							fill-opacity='1'
+							d='M0,160L60,176C120,192,240,224,360,229.3C480,235,600,213,720,181.3C840,149,960,107,1080,106.7C1200,107,1320,149,1380,170.7L1440,192L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z'></path>
+					</svg>
+				</div>
+			) : null}
 			<Navigation
 				prev={{ name: '', path: '' }}
 				next={{ name: 'about', path: '/me' }}
