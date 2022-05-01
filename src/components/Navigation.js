0432a524b6ee9582fdd2761a88790dd96442059f
Nuriddin Islamov <nuriddin.k2003@gmail.com>
2022-05-01 09:33:23 +0100

    add margins for navigation button

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -6,7 +6,7 @@ function Navigation(props) {
 		'border-2 border-gray-600 px-3 py-1 my-2 rounded hover:bg-gray-800 hover:border-gray-800 hover:text-white whitespace-nowrap text-lg font-bold';
 
 	return (
-		<div className='h-20 flex flex-wrap-reverse justify-between items-center px-12 md:hidden'>
+		<div className='mt-6 h-20 flex flex-wrap-reverse justify-between items-center px-12 md:hidden'>
 			{props.prev.path === '' ? null : (
 				<div className={navClasses}>
 					<Link to={props.prev.path}>&larr; {props.prev.name}</Link>
