277a9acffdff19dec038fd6929512f86ffc0a172
Nuriddin Islamov <nuriddin.k2003@gmail.com>
2022-05-02 14:02:25 +0100

    Resize navigation buttons

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -3,10 +3,10 @@ import { Link } from 'react-router-dom';
 
 function Navigation(props) {
 	const navClasses =
-		'border-2 border-gray-600 px-3 py-1 my-2 rounded hover:bg-gray-800 hover:border-gray-800 hover:text-white whitespace-nowrap text-lg font-bold';
+		'border-2 border-gray-600 px-2 py-1 rounded hover:bg-gray-800 hover:border-gray-800 hover:text-white whitespace-nowrap text-md font-bold';
 
 	return (
-		<div className='mt-6 h-20 flex flex-wrap-reverse justify-between items-center px-12 md:hidden'>
+		<div className='mt-6 h-20 flex flex-wrap justify-between items-center px-12 md:hidden'>
 			{props.prev.path === '' ? null : (
 				<div className={navClasses}>
 					<Link to={props.prev.path}>&larr; {props.prev.name}</Link>
