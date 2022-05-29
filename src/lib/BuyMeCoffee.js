377f4eb74d185c10d2dea19cc5fa74753a746303
Nuriddin Islamov <nuriddin.k2003@gmail.com>
2022-05-29 14:50:55 +0100

    Add icon to BMAC button - BMAC: buy me a coffee

--- src/lib/BuyMeCoffee.js
+++ src/lib/BuyMeCoffee.js
@@ -1,15 +1,20 @@
 import React from 'react';
+import icon from '../assets/buymeacoffee.svg';
 
 function BuyMeCoffee({ width, height }) {
 	return (
 		<div className='mt-8 mb-4 flex justify-center items-center'>
-			<a
-				className={`px-4 py-2 w-${width} h-${height} text-center border-2 solid rounded border-gray-800 bg-yellow-300 no-underline hover:drop-shadow-lg after:bg-red-500`}
-				href='https://www.buymeacoffee.com/nuriddinislamov'
-				target='_blank'
-				type='button'>
-				Buy me a cup of coffee
-			</a>
+			<div
+				className={`px-4 py-2 w-${width} h-${height} text-center border-2 solid rounded border-gray-800 bg-yellow-300 hover:drop-shadow-lg after:bg-red-500 flex justify-center items-center font-mono`}>
+				<img src={icon} className='my-0 mx-2' />
+				<a
+					className='no-underline'
+					href='https://www.buymeacoffee.com/nuriddinislamov'
+					target='_blank'
+					type='button'>
+					Buy me a cup of coffee
+				</a>
+			</div>
 		</div>
 	);
 }
