8f173da5d8f23d6a17f10cb33f18bb3d101035bb
Nuriddin Islamov <nuriddin.k2003@gmail.com>
2022-05-29 14:53:43 +0100

    fix react warnings

--- src/lib/BuyMeCoffee.js
+++ src/lib/BuyMeCoffee.js
@@ -6,11 +6,12 @@ function BuyMeCoffee({ width, height }) {
 		<div className='mt-8 mb-4 flex justify-center items-center'>
 			<div
 				className={`px-4 py-2 w-${width} h-${height} text-center border-2 solid rounded border-gray-800 bg-yellow-300 hover:drop-shadow-lg after:bg-red-500 flex justify-center items-center font-mono`}>
-				<img src={icon} className='my-0 mx-2' />
+				<img src={icon} alt='Coffee icon' className='my-0 mx-2' />
 				<a
 					className='no-underline'
 					href='https://www.buymeacoffee.com/nuriddinislamov'
 					target='_blank'
+					rel='noopener noreferrer'
 					type='button'>
 					Buy me a cup of coffee
 				</a>
