d46aef5b25cf9881623b3ab1db8c360260501d48
Nuriddin Islamov <nuriddin.k2003@gmail.com>
2022-07-09 16:55:23 +0100

    update coffee button link hover

--- src/lib/BuyMeCoffee.js
+++ src/lib/BuyMeCoffee.js
@@ -3,20 +3,18 @@ import icon from '../assets/buymeacoffee.svg';
 
 function BuyMeCoffee({ width, height }) {
 	return (
-		<div className='mt-8 mb-4 flex justify-center items-center'>
+		<a
+			href='https://www.buymeacoffee.com/nuriddinislamov'
+			target='_blank'
+			rel='noopener noreferrer'
+			type='button'
+			className='mt-8 mb-4 flex justify-center items-center no-underline'>
 			<div
 				className={`px-4 py-2 w-${width} h-${height} text-center border-2 solid rounded border-gray-800 bg-yellow-300 hover:drop-shadow-lg after:bg-red-500 flex justify-center items-center font-mono`}>
 				<img src={icon} alt='Coffee icon' className='my-0 mx-2' />
-				<a
-					className='no-underline'
-					href='https://www.buymeacoffee.com/nuriddinislamov'
-					target='_blank'
-					rel='noopener noreferrer'
-					type='button'>
-					Buy me a cup of coffee
-				</a>
+				<span>Buy me a cup of coffee</span>
 			</div>
-		</div>
+		</a>
 	);
 }
 
