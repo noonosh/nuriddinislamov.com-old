305777ded3ad328801bced3ce15c5685604ed0a6
Nuriddin Islamov <nuriddin.k2003@gmail.com>
2022-07-09 20:39:44 +0100

    fix button styling

--- src/lib/BuyMeCoffee.js
+++ src/lib/BuyMeCoffee.js
@@ -3,18 +3,16 @@ import icon from '../assets/buymeacoffee.svg';
 
 function BuyMeCoffee({ width, height }) {
 	return (
-		<a
-			href='https://www.buymeacoffee.com/nuriddinislamov'
-			target='_blank'
-			rel='noopener noreferrer'
-			type='button'
-			className='mt-8 mb-4 flex justify-center items-center no-underline'>
-			<div
-				className={`px-4 py-2 w-${width} h-${height} text-center border-2 solid rounded border-gray-800 bg-yellow-300 hover:drop-shadow-lg after:bg-red-500 flex justify-center items-center font-mono`}>
+		<div className='mt-8 mb-4 flex justify-center items-center'>
+			<a
+				href='https://www.buymeacoffee.com/nuriddinislamov'
+				target='_blank'
+				rel='noopener noreferrer'
+				className={`px-4 py-2 w-${width} h-${height} text-center border-2 solid rounded border-gray-800 bg-yellow-300 hover:drop-shadow-lg after:bg-red-500 flex justify-center items-center font-mono no-underline`}>
 				<img src={icon} alt='Coffee icon' className='my-0 mx-2' />
 				<span>Buy me a cup of coffee</span>
-			</div>
-		</a>
+			</a>
+		</div>
 	);
 }
 
