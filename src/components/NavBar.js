2e7c993117ae3d43a52b63a3197311b09d3301a7
Nuriddin Islamov <nuriddin.k2003@gmail.com>
2022-10-31 22:49:45 +0000

    update navbar - remove projects link - add new banner with link to my blog

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,14 +1,16 @@
 import React from "react";
 import useWindowDimensions from "../lib/windowDimensions";
 import { Link } from "react-router-dom";
+import Banner from "../components/Banner";
 
 const NavBar = () => {
 	const { width } = useWindowDimensions();
 
 	return (
-		<header className='md:sticky top-0 z-30'>
-			<nav className='pt-6 md:pt-8 px-24 bg-white w-full bg-opacity-90 backdrop-blur-md'>
-				<div className='flex flex-col items-center md:flex-row md:justify-between border-b pb-8'>
+		<header className='md:sticky w-full top-0 z-30'>
+			<Banner />
+			<nav className='pt-2 md:pt-4 px-24 bg-white w-full bg-opacity-90 backdrop-blur-md'>
+				<div className='flex flex-col items-center md:flex-row md:justify-between border-b pb-4'>
 					<div className='text-center md:text-left'>
 						<h1 className='font-extrabold text-2xl md:text-4xl md:self-center md:mb-0 inline-block'>
 							<Link to='/' className='link'>
@@ -24,11 +26,6 @@ const NavBar = () => {
 						<li className='nav_item'>
 							<Link className='link' to='/me'>
 								me
-							</Link>
-						</li>
-						<li className='nav_item'>
-							<Link className='link' to='/projects'>
-								projects
 							</Link>
 						</li>
 						<li className='nav_item'>
