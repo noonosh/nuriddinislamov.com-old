0f80a742241a33d1e20bac2e1c9f1f639dd81352
Nuriddin Islamov <nuriddin.k2003@gmail.com>
2022-10-31 22:55:03 +0000

    new tab to link to blog

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -6,6 +6,8 @@ const Banner = () => {
 	return (
 		<a
 			href='https://read.nuriddinislamov.com/'
+			target='_blank'
+			rel='noopener noreferrer'
 			className='block w-screen bg-blue-600 py-2 sticky top-0 left-0 hover:underline underline-offset-2 text-white'>
 			<h4 className='uppercase text-center animate__animated animate-pulse'>
 				Read more posts on my blog page
