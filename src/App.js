7445d29e822556a85bac0a2414d69f9038736767
Nuriddin Islamov <nuriddin.k2003@gmail.com>
2022-10-31 22:49:23 +0000

    fix routes in app file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import React from 'react';
-import Home from './components/Home';
-import About from './components/About';
-import Work from './components/Work';
-import Contact from './components/Contact.js';
-// import Post from './components/Post.js';
-import NotFound from './components/404';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React from "react";
+import Home from "./components/Home";
+import About from "./components/About";
+import Contact from "./components/Contact.js";
+import NotFound from "./components/404";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const App = () => {
 	return (
@@ -14,9 +12,7 @@ const App = () => {
 				<Routes>
 					<Route path='/' element={<Home />} />
 					<Route path='/me' element={<About />} />
-					<Route path='/projects' element={<Work />} />
 					<Route path='/reach-out' element={<Contact />} />
-					{/* <Route path='/posts/:id' element={<Post />} /> */}
 					<Route path='*' element={<NotFound />} />
 				</Routes>
 			</BrowserRouter>
